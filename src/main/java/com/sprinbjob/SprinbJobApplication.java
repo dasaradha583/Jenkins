6adaaace65dfd0327256b99c911f431c8cea6a0f
dasaradha583 <dasaradharamireddy583@gmail.com>
2025-10-15 18:35:38 +0530

    first commit:

---
+++ src/main/java/com/sprinbjob/SprinbJobApplication.java
@@ -0,0 +1,13 @@
+package com.sprinbjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinbJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprinbjob/SprinbJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprinbjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
